1eec3a51850f4c89b9b6d8c5deb60795bb7ec4c7
rioto.nagano <kingdom950927@gmail.com>
2022-11-20 13:27:13 +0900

    変更

--- app/models/line_food.rb
+++ app/models/line_food.rb
@@ -6,9 +6,9 @@ class LineFood < ApplicationRecord
   validates :count, numericality: { greater_than: 0 }
 
   scope :active, -> { where(active: true) }
-  scope :other_restaurant, -> (picked_restaurant_id) { where.not(restaurant_id: picked_restaurant_id)}
+  scope :other_restaurant, -> (picked_restaurant_id) { where.not(restaurant_id: picked_restaurant_id) }
 
   def total_amount
-    food.price*count
+    food.price * count
   end
 end
