ec21459b8ab3df71fd955aba5cf8d39968c93f79
rioto.nagano <kingdom950927@gmail.com>
2022-11-20 13:28:31 +0900

    変更

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,13 +3,12 @@ class Order < ApplicationRecord
 
   validates :total_price, numericality: { greater_than: 0 }
 
-
   def save_with_update_line_foods!(line_foods)
     ActiveRecord::Base.transaction do
       line_foods.each do |line_food|
-        line_food.update!(active: false,  order: self)
+        line_food.update_attributes!(active: false, order: self)
       end
-      self.save_with_update_line_foods!
+      self.save!
     end
   end
 end
